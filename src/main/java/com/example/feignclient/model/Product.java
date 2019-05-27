0c6c13337931a3287e45b8a9e10bf9b25d229358
Deerghayu Shrestha <deerghayu.shrestha@gmail.com>
2019-05-27 14:22:35 -0500

    S000000 - FeignClient app init

---
+++ src/main/java/com/example/feignclient/model/Product.java
@@ -0,0 +1,72 @@
+package com.example.feignclient.model;
+
+
+public class Product {
+
+    private String id;
+    private String name;
+    private String type;
+    private String category;
+    private Double price;
+    private String Description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", Description='" + Description + '\'' +
+                '}';
+    }
+}
